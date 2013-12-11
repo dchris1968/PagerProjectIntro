71011fb9b46e8c3bcd3e45dbf843b8b74ccfeac1
dchris1968 <dchris@yahoo.com>
2013-12-10 22:39:23 -0500

    Comments Added
    Added comments and cleaned up code some more.

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+/*******************************
+ **     PagerProjectIntro     **
+ **     December 10, 2014     **
+ **	  Douglass C. De Antonio  **
+ *******************************/
+
+
 //These lines get the database ready and puts it in the $con string
 $con = mysqli_connect("127.0.0.1", "root","","cars");
 $sql = "SELECT count(id) FROM car";
@@ -19,6 +26,7 @@ if($last < 1)
 {
 	$last = 1;
 }
+
 //Establish the $pagenum variable
 $pagenum =1;
 
@@ -27,6 +35,7 @@ if(isset($_GET['pn']))
 {
 	$pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
 }
+
 //This makes sure the page number isn't below 1, or more than our $last page
 if($pagenum < 1) 
 {
@@ -36,16 +45,13 @@ else if ($pagenum > $last)
 {
 	$pagenum = $last;
 }
+
 //This sets the range of rows to query for the chosen $pagenum
 $limit = 'LIMIT '.($pagenum -1) * $page_rows.','.$page_rows;
 
 //This is your query again, it is for grabbing just one page worth of rows by applying $limit
 $sql = "SELECT * FROM car $limit;";
 $query = mysqli_query($con, $sql);
-
-//This shows the user what page they are on, and the total number of pages
-$textline1 = "Cars (<b>$rows</b>)";
-$textline2 = "Page <b>$pagenum</b> of <b>$last</b>";
 
 //Establish the $paginationCtrls variable
 $paginationCtrls = '';
@@ -58,54 +64,67 @@ if($last != 1)
 	/* First we check if we are on page one.  If we are then we don't need a link to
 	   the previous page or the first page so we do nothing.  If we aren't then we 
 	   generate links to the first page, and to the previous page. */
-	if ($pagenum > 1) {
+	if ($pagenum > 1) 
+	{
 		$previous = $pagenum - 1;
 		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$first.'">First</a> &nbsp; &nbsp;';
 		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previous.'">Previous</a> &nbsp; &nbsp;';
 		
-		
 		//Render clickable number links that should appear on the left of the target page number
+		//If the $pagenum is equal to $last, it puts four links to the left of it
 		if($pagenum == $last)
 		{
 			for($i = $pagenum-4; $i < $pagenum; $i++)
 			{
-				if($i > 0){
+				if($i > 0)
+				{
 					$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> &nbsp;';
 				}
 			}
 		}
+		
+		//If the $pagenum is one from $last, it puts three links to the left of it
 		if($pagenum == $last-1)
 		{
 			for($i = $pagenum-3; $i < $pagenum; $i++)
 			{
-				if($i > 0){
+				if($i > 0)
+				{
 					$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> &nbsp;';
 				}
 			}
 		}
+		
+		//If the $pagenum is 2 from $last, it puts two links to the left of it
 		if($pagenum == $last-2)
 		{
 			for($i = $pagenum-2; $i < $pagenum; $i++)
 			{
-				if($i > 0){
+				if($i > 0)
+				{
 					$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> &nbsp;';
 				}
 			}
 		}
+		
+		//If neither of the previous if statements are true, the following will run
 		if(($pagenum != $last) && ($pagenum != $last-1) && ($pagenum != $last-2))
 		{
 			for($i = $pagenum-2; $i < $pagenum; $i++)
 			{
-				if($i > 0){
+				if($i > 0)
+				{
 					$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> &nbsp;';
 				}
 			}
 		}
 	}
+	
 	//Render the target page number, but without it being a link
 	$paginationCtrls .= ''.$pagenum.' &nbsp; ';
 	
 	//Render clickable number links that should appear on the right of the target page number
+	//If $pagenum is on the first link, add four links to the right of it
 	if($pagenum == 1)
 	{
 		for($i = $pagenum+1; $i <= $last; $i++)
@@ -118,6 +137,7 @@ if($last != 1)
 		}
 	}
 	
+	//If $pagenum is on the second link, add three links to the right of it
 	if($pagenum == 2)
 	{
 		for($i = $pagenum+1; $i <= $last; $i++)
@@ -130,18 +150,20 @@ if($last != 1)
 		}
 	}
 	
+	//If $pagenum is on the third or more link, add two links to the right of it
 	if($pagenum > 2)
 	{
 		for($i = $pagenum+1; $i <= $last; $i++)
 		{
 			$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> &nbsp;';
-			if($i >= $pagenum+2){
+			if($i >= $pagenum+2)
+			{
 				break;
 			}
 		}
 	}
-	//This does the same as above, only checking if we are on the last page, and then generating the "Next" and "Last"
 	
+	//This checks to see if we are on the last page, and then generates the "Next" and "Last" links
 	if($pagenum != $last)
 	{
 		$next = $pagenum + 1;
@@ -150,15 +172,20 @@ if($last != 1)
 	}
 	
 }
+	//Sets the $list to generate the fields to be displayed
 	$list = '';
+	
+	//Sets up the table and borders
 	echo "<table border='1'>
-	<tr>
-	<th>ID</th>
-	<th>Make</th>
-	<th>Model</th>
-	<th>Year</th>
-	<th>Color</th>
-	</tr>";
+		<tr>
+			<th>ID</th>
+			<th>Make</th>
+			<th>Model</th>
+			<th>Year</th>
+			<th>Color</th>
+		</tr>";
+		
+	//Accesses the fields to be displayed
 	while($row = mysqli_fetch_array($query, MYSQLI_ASSOC))
 	{
 		echo "<tr>";
@@ -168,8 +195,11 @@ if($last != 1)
 		echo "<td>" . $row['year'] . "</td>";
 		echo "<td>" . $row['color'] . "</td>";
 	}
+	
+	//Ends the table
 	echo "</table>";
-//Close your database connection
+	
+//Closes the cars database connection
 mysqli_close($con);
 
 ?>
@@ -192,7 +222,10 @@ mysqli_close($con);
 		
 		<?php echo $list; ?> 
 		
-		<div id="pagination_controls"><?php echo $paginationCtrls; ?></div>
+		<div id="pagination_controls">
+			<?php echo $paginationCtrls; ?>
+		</div>
+		
 	</div>
 
 </body>
